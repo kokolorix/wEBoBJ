2974e9f96631f1d6a8f8065c397a875bdd5d4298
Simon Reber <simon.reber@brunnerinformatik.ch>
2020-10-19 15:41:23 +0000

    Mit 'neuem' Design

--- src/app/prototypes/projektliste4/projektliste4.component.ts
+++ src/app/prototypes/projektliste4/projektliste4.component.ts
@@ -3,10 +3,18 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
+export interface Status
+{
+  titel:string;
+  icon:string;
+  class:string;
+}
 export interface Projekt{
   titel:string;
   vnb:string;
   art:string;
+  statusText:string;
+  status:Status[];
 }
 
 @Component({
@@ -15,7 +23,7 @@ export interface Projekt{
   styleUrls: ['./projektliste4.component.scss']
 })
 export class Projektliste4Component implements OnInit {
-  displayedColumns: string[] = ['titel', 'art', 'vnb'];
+  displayedColumns: string[] = ['titel', 'art', 'vnb', 'status'];
   dataSource: MatTableDataSource<Projekt>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -23,7 +31,7 @@ export class Projektliste4Component implements OnInit {
 
   constructor() {
       // Create 100 users
-      const projekte = Array.from({length: 2500}, (_, k) => erstelleProjekt(k + 1));
+      const projekte = Array.from({length: 250}, (_, k) => erstelleProjekt(k + 1));
 
       // Assign the data to the data source for the table to render
       this.dataSource = new MatTableDataSource(projekte);
@@ -60,9 +68,15 @@ const VNBs: string[] = [
 const ORTE: string[] = [
   '3257 Walddorf', '6587 Strandstadt', '4587 Hauptstadt', '4107 Hinterwald'
 ];
-const PROEKTARTEN: string[] = [
+const AUFTRAG_DEFS: string[] = [
   'Neubau', 'Umbau', 'period. Kontrolle', 'temp. Anlage'
 ];
+const LEIST_DEFS: string[] = [
+  'TAG', 'IA', 'AB', 'SiNa','MPP','Schema','Beilage',
+];
+const STATI: string[] = [
+  'stWorking','stSent','stReceived','stApproved',
+]
 
 function erstelleProjekt(id: number) : Projekt
 {
@@ -70,11 +84,40 @@ function erstelleProjekt(id: number) : Projekt
   const ort: string = ORTE[Math.round(Math.random() * (ORTE.length - 1))];
   const nr : string = Math.round(Math.random() * (STRASSEN.length - 1) + 1).toString();
   const vnb : string = VNBs[Math.round(Math.random() * (VNBs.length - 1))];
-  const art : string = PROEKTARTEN[Math.round(Math.random() * (PROEKTARTEN.length - 1))];
+  const art : string = AUFTRAG_DEFS[Math.round(Math.random() * (AUFTRAG_DEFS.length - 1))];
+
+  var status : Status[] = [];
+  var classes = new Set();
+
+  for(let i = 0; i < LEIST_DEFS.length; ++i) {
+    let x = Math.round(Math.random() * 9) ;
+    let def = LEIST_DEFS[i];
+
+    if(x && (x % 2)){
+      const cls : string = STATI[Math.round(Math.random() * (STATI.length - 1))];
+      status.push({titel:`${def}`, icon:`filter_${x}`, class: cls});
+      classes.add(cls);
+    }
+  }
+
+  let statusText:string;
+
+    if(classes.has('stWorking'))
+      statusText = 'In Arbeit';
+    else if(classes.has('stReceived'))
+      statusText = 'Rückmeldung';
+    else if(classes.has('stSent'))
+      statusText = 'Gesendet';
+    else if(classes.has('stApproved'))
+      statusText = 'Abgeschlossen';
+    else
+      statusText = 'Neu'
 
   return {
     titel : strasse + ' ' + nr + ', ' + ort,
     vnb: vnb,
     art: art,
+    statusText: statusText,
+    status: status,
   }
 }
