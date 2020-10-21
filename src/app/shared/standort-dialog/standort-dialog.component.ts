842c3f562c9d305a201ed366dae9cd63d02df488
Simon Reber <sr.dev@outlook.com>
2020-10-21 17:56:03 +0000

    Projekt-Detail geht ein bisschen mehr

---
+++ src/app/shared/standort-dialog/standort-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-standort-dialog',
+  templateUrl: './standort-dialog.component.html',
+  styleUrls: ['./standort-dialog.component.scss']
+})
+export class StandortDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
