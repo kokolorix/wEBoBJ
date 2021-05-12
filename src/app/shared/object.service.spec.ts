abe739c175e3d7e6e09650c1c1ca4d7723aec5ea
srdev <sr.dev@outlook.com>
2021-05-13 00:14:30 +0200

    tests are running again

--- src/app/shared/object.service.spec.ts
+++ src/app/shared/object.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ObjectService } from './object.service';
+import { Object, Property, Obj, EmptyT } from 'src/app/shared/object';
 
 describe('ObjectService', () => {
   let service: ObjectService;
+  let object: Object;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ObjectService);
+    object = new Obj();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('empty object should have EmptyT as type',() => {
+    let emptyT = new EmptyT();
+    let isEmptyT = object.type.id.toString() === emptyT.id.toString();
+    expect(isEmptyT).toBeFalse();
+  });
+
 });
