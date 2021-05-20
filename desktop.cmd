f784055324858e41538d9a669362df545f98750c
Simon Reber <simon.reber@brunnerinformatik.ch>
2021-05-20 19:13:52 +0200

    desktop, more refined

--- desktop.cmd
+++ desktop.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-
+for %%i in (.) do set fn=%%~nxi
 
 attrib . | find "R"
 if errorlevel 1 goto:r+
@@ -8,6 +8,13 @@ if errorlevel 0 goto:r-
 
 :r+
 echo +r
+if exist desktop.ini goto:r++
+echo [.ShellClassInfo] > desktop.ini
+echo ConfirmFileOp=0 >> desktop.ini
+echo IconFile=folder.ico >> desktop.ini
+echo IconIndex=0 >> desktop.ini
+echo InfoTip="%fn%" >> desktop.ini
+:r++
 attrib +r .
 attrib +h desktop.ini
 attrib +h %~nx0
